81fc001cc1f20f939743b10abe5dac5a4ca6cc0a
fumtas1k <fumta01a7@gmail.com>
2022-06-06 14:05:32 +0900

    add:入出金一覧表示機能追加

--- assets/js/indexedDB.js
+++ assets/js/indexedDB.js
@@ -93,11 +93,62 @@ const insertData = (balance, date, category, amount, memo) => {
     let addData = store.add(data);
     addData.onsuccess = () => {
       console.log("データが登録できました");
-      alert("登録しました");
+      // 入出金一覧を作成
+      createList();
     }
     addData.onerror = () => {
       console.log("データが登録できませんでした");
     }
     db.close();
+  }
+}
+
+const createList = () => {
+  let database = indexedDB.open(dbName, dbVersion);
+  database.onsuccess = (event) => {
+    let db = event.target.result;
+    let transaction = db.transaction(storeName, "readonly");
+    let store = transaction.objectStore(storeName);
+    store.getAll().onsuccess = (data) => {
+      console.log(data);
+      let rows = data.target.result;
+
+      let section = document.getElementById("list");
+      // 入出金一覧のテーブルを作成
+      let table = `
+        <table>
+          <thead>
+            <tr>
+              <th>日付</th>
+              <th>収支</th>
+              <th>カテゴリ</th>
+              <th>金額</th>
+              <th>メモ</th>
+              <th>削除</th>
+            </tr>
+          </thead>
+          <body>
+      `;
+
+      // 入出金のデータを表示
+      rows.forEach((element) => {
+        console.log(element);
+        table += `
+          <tr>
+            <td>${element.date}</td>
+            <td>${element.balance}</td>
+            <td>${element.category}</td>
+            <td>${element.amount}</td>
+            <td>${element.memo}</td>
+            <td><button>x</button></td>
+          </tr>
+        `;
+      });
+      table += `
+          </tbody>
+        </table>
+      `;
+      section.innerHTML = table;
+    }
   }
 }
